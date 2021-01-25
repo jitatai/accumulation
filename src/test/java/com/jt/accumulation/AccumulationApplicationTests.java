47fd22f8adfefa62f5389294a2da99170cb655c1
jitatai <846722168@qq.com>
2021-01-25 10:47:07 +0800

    Initial commit

---
+++ src/test/java/com/jt/accumulation/AccumulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt.accumulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccumulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
