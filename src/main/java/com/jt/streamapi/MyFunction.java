4ad2b4a812f0e0e9dd9721f540c79d5ff7b66f32
jitatai <846722168@qq.com>
2021-02-19 17:35:35 +0800

    山寨StreamApi

---
+++ src/main/java/com/jt/streamapi/MyFunction.java
@@ -0,0 +1,19 @@
+package com.jt.streamapi;
+
+/**
+ * @author jiatai.hu
+ * @version 1.0
+ * @date 2021/2/19 17:08
+ * E(enter) 入参类型
+ * R(return) 返回值类型
+ */
+@FunctionalInterface
+public interface MyFunction<E,R> {
+    /**
+     * 接受E类型 返回 R类型，也就是把E映射成R
+     * @param e
+     * @return
+     */
+    R apply(E e);
+//    E apply2(R r);
+}
---
+++ src/main/java/com/jt/streamapi/MyMockStream.java
@@ -0,0 +1,61 @@
+package com.jt.streamapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jiatai.hu
+ * @version 1.0
+ * @date 2021/2/19 17:22
+ */
+public class MyMockStream {
+    public static void main(String[] args) {
+        MyList<Person> myList =new MyList<Person>();
+        myList.add(new Person(18,"jiatai"));
+        myList.add(new Person(18,"jiatai"));
+        myList.add(new Person(18,"jiatai"));
+
+    }
+
+    static class MyList<E>{
+        private List<E> list = new ArrayList<>();
+
+        public void add(E e){
+            list.add(e);
+        }
+
+        public List<E> filter(Predicate<E> filter){
+            List filteredList = new ArrayList();
+            for (E e : list) {
+                if (filter.test(e)){
+                    filteredList.add(e);
+                }
+            }
+            return filteredList;
+        }
+
+        public <R> List<R> map(Function<E,R> mapper){
+            List<R> mapperList = new ArrayList<>();
+            for (E e : list) {
+                mapperList.add(mapper.mapper(e));
+            }
+            return mapperList;
+        }
+    }
+    interface Predicate<E>{
+        boolean test(E e);
+    }
+    interface Function<E,R>{
+        R mapper(E e);
+    }
+    @Setter
+    @AllArgsConstructor
+    static class Person{
+        int age;
+        String name;
+    }
+}
+
