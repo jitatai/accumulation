14b7972ae9f7d0dbeaf90d3b4f236d181f9c7619
jitatai <846722168@qq.com>
2021-03-03 09:38:09 +0800

    同源策略与跨域

--- src/main/java/com/jt/web/crossorigin/controller/UserCrossOriginController.java
+++ src/main/java/com/jt/web/crossorigin/controller/UserCrossOriginController.java
@@ -1,9 +1,7 @@
 package com.jt.web.crossorigin.controller;
 
-import com.jt.web.crossorigin.entity.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,18 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserCrossOriginController {
 
-    @GetMapping(value = "/getUser/{id}")
-    @CrossOrigin("http://localhost:7070")
-    public User getUser(@PathVariable("id") Long id) {
-        // id没用上，就是演示一下@PathVariable注解
-        System.out.println("id:" + id);
-
-        User user = new User();
-        user.setName("bravo");
-        user.setAge(18);
-        user.setAddress("wenzhou");
-
-//        return user;
-        return user;
+    @GetMapping(value = "/getCodeMsg")
+    public String getUser() throws JsonProcessingException {
+        String data = HttpRequest.sendGet("http://191.168.0.95:9000/API/service/MakeCode", "");
+        System.out.println(data);
+        return data;
     }
 }
