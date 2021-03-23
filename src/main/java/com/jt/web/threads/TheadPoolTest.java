6aa8420fae9a2b09da20cd9fb9831468980c6fa5
jitatai <846722168@qq.com>
2021-03-23 14:46:15 +0800

    线程池测试

--- src/main/java/com/jt/web/threads/TheadPoolTest.java
+++ src/main/java/com/jt/web/threads/TheadPoolTest.java
@@ -6,4 +6,10 @@ package com.jt.web.threads;
  * @date 2021/3/23 10:10
  */
 public class TheadPoolTest {
+    /**
+     * 继承体系：Executor 接口
+     * ExecutorService 继承Executor
+     * AbstractExecutorService 实现ExecutorService  ScheduledExecutorService 继承ExecutorService
+     * ThreadPoolExecutor 实现AbstractExecutorService  ForkJoinPool
+     */
 }
